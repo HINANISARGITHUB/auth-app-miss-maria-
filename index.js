bf1fc7432304401384c14ab9ddbe4d0ccca58480
Hina Nisar <160288150+HINANISARGITHUB@users.noreply.github.com>
2026-05-04 16:20:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const app = express();
 
 // ✅ CORS (Frontend URL yahan lazmi dalen jab deploy ho jaye)
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin:"https://auth-app-miss-maria-frontend.vercel.app",methods:["GET,"POST","PUT","DELETE"],credentials:true}));
 
 // ✅ MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
